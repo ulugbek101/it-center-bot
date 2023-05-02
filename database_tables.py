a49bdffcb20532980f0576a4f8652cb7cd052bfe
snd <viper.ghost@bk.ru>
2023-05-03 00:18:24 +0500

    Add database_tables.py file where stored functions to create and setup all database tables

---
+++ database_tables.py
@@ -0,0 +1,20 @@
+from loader import db
+
+
+def create_users_table() -> None:
+    """
+    Create Users table in database
+    :return: None
+    """
+    sql = """
+        CREATE TABLE IF NOT EXISTS Users(
+            user_id INT PRIMARY KEY AUTO_INCREMENT,
+            chat_id INT NOT NULL UNIQUE,
+            username VARCHAR(255),
+            fullname VARCHAR(255)
+        );
+    """
+    db.execute(sql, commit=True)
+
+
+create_users_table()
