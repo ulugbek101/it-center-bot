08c11c31ca4c4f9a25c5ea68f6be93105e5927dd
snd <viper.ghost@bk.ru>
2023-05-04 00:50:34 +0500

    Add Courses table

--- database_tables.py
+++ database_tables.py
@@ -17,4 +17,22 @@ def create_users_table() -> None:
     db.execute(sql, commit=True)
 
 
+def create_courses_table() -> None:
+    """
+    Create Courses table in a database
+    :return: None
+    """
+    sql = """
+         CREATE TABLE IF NOT EXISTS Courses(
+            course_id INT PRIMARY KEY AUTO_INCREMENT,
+            course_name VARCHAR(255) NOT NULL UNIQUE,
+            course_desc_path VARCHAR(255),
+            course_image_path VARCHAR(255)
+        );
+    """
+    db.execute(sql, commit=True)
+
+
 # create_users_table()
+# create_courses_table()
+
